b0839dd0556ada914000b8782257fe963d034a18
Stefan Gaertner <sgaertner@cgs-online.de>
2020-12-21 09:22:31 +0100

    slight refactoring

--- AOC2020/src/main/java/net/stefangaertner/aoc20/Day21.java
+++ AOC2020/src/main/java/net/stefangaertner/aoc20/Day21.java
@@ -21,17 +21,47 @@ public class Day21 {
 		Advent.print(2, part2());
 	}
 
+	private static List<Set<String>> ingredientsList = new ArrayList<>();
+	private static Map<String, Set<Set<String>>> a2i = new HashMap<>();
+	private static Map<String, Set<String>> i2a = new HashMap<>();
+
 	static long part1() {
 		List<String> lines = FileUtils.read("aoc20/021");
 
-		List<Set<String>> ingredientsList = new ArrayList<>();
-		Map<String, Set<Set<String>>> a2i = new HashMap<>();
-		Map<String, Set<String>> i2a = new HashMap<>();
+		parse(lines);
 
-		lines.forEach(line -> {
-			if (!line.contains("(")) {
-				System.err.println(line);
-			}
+		remove(i2a, a2i);
+
+		return i2a.entrySet()
+				.stream()
+				.filter(e -> e.getValue()
+						.isEmpty())
+				.map(e -> {
+					String ingredient = e.getKey();
+					return ingredientsList.stream()
+							.filter(set -> set.contains(ingredient))
+							.count();
+				})
+				.reduce(0L, (a, b) -> a + b);
+	}
+
+	static String part2() {
+		List<String> lines = FileUtils.read("aoc20/021");
+	
+		parse(lines);
+	
+		remove(i2a, a2i);
+	
+		Map<String, String> out = evaluate(i2a, a2i);
+	
+		return out.entrySet()
+				.stream()
+				.map(Map.Entry::getValue)
+				.collect(Collectors.joining(","));
+	}
+
+	private static void parse(List<String> lines) {
+		for (String line : lines) {
 			String p = "(.*) \\(contains (.*)\\)";
 			String in1 = RegexUtil.first(line, p);
 			String in2 = RegexUtil.second(line, p);
@@ -54,61 +84,33 @@ public class Day21 {
 				i2a.get(ingredient)
 						.addAll(allergens);
 			}
-		});
-
-		remove(i2a, a2i);
-
-		return i2a.entrySet()
-				.stream()
-				.filter(e -> e.getValue()
-						.isEmpty())
-				.map(e -> {
-					String ingredient = e.getKey();
-					return ingredientsList.stream()
-							.filter(set -> set.contains(ingredient))
-							.count();
-				})
-				.reduce(0L, (a, b) -> a + b);
+		}
 	}
 
-	static String part2() {
-		List<String> lines = FileUtils.read("aoc20/021");
-
-		Map<String, Set<Set<String>>> a2i = new HashMap<>();
-		Map<String, Set<String>> i2a = new HashMap<>();
-
-		for (String line : lines) {
-			String p = "(.*) \\(contains (.*)\\)";
-			String in1 = RegexUtil.first(line, p);
-			String in2 = RegexUtil.second(line, p);
-
-			Set<String> ingredients = Arrays.stream(in1.split(" "))
-					.collect(Collectors.toSet());
-
-			Set<String> allergens = Arrays.stream(in2.split(", "))
-					.collect(Collectors.toSet());
-
-			for (String allergen : allergens) {
-				a2i.putIfAbsent(allergen, new HashSet<>());
-				a2i.get(allergen)
-						.add(ingredients);
-			}
-
-			for (String ingredient : ingredients) {
-				i2a.putIfAbsent(ingredient, new HashSet<>());
-				i2a.get(ingredient)
-						.addAll(allergens);
-			}
-		}
-
-		remove(i2a, a2i);
-
-		Map<String, String> out = evaluate(i2a, a2i);
-
-		return out.entrySet()
+	private static void remove(Map<String, Set<String>> i2a, Map<String, Set<Set<String>>> a2i) {
+	
+		Set<String> ingredients = i2a.keySet()
 				.stream()
-				.map(Map.Entry::getValue)
-				.collect(Collectors.joining(","));
+				.collect(Collectors.toSet());
+	
+		ingredients.forEach(ingredient -> {
+			Set<String> possibleAllergens = i2a.get(ingredient)
+					.stream()
+					.collect(Collectors.toSet());
+	
+			// check if it's in all lists
+			for (String allergen : possibleAllergens) {
+				boolean possible = a2i.get(allergen)
+						.stream()
+						.allMatch(set -> set.contains(ingredient));
+				if (!possible) {
+					i2a.get(ingredient)
+							.remove(allergen);
+				}
+			}
+	
+		});
+	
 	}
 
 	private static Map<String, String> evaluate(Map<String, Set<String>> i2a, Map<String, Set<Set<String>>> a2i) {
@@ -154,32 +156,6 @@ public class Day21 {
 		}
 
 		return out;
-	}
-
-	private static void remove(Map<String, Set<String>> i2a, Map<String, Set<Set<String>>> a2i) {
-
-		Set<String> ingredients = i2a.keySet()
-				.stream()
-				.collect(Collectors.toSet());
-
-		ingredients.forEach(ingredient -> {
-			Set<String> possibleAllergens = i2a.get(ingredient)
-					.stream()
-					.collect(Collectors.toSet());
-
-			// check if it's in all lists
-			for (String allergen : possibleAllergens) {
-				boolean possible = a2i.get(allergen)
-						.stream()
-						.allMatch(set -> set.contains(ingredient));
-				if (!possible) {
-					i2a.get(ingredient)
-							.remove(allergen);
-				}
-			}
-
-		});
-
 	}
 
 }
