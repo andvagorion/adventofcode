5626898b014a7c86fae3bb7a0f7ba37ad606942e
Stefan Gaertner <sgaertner@cgs-online.de>
2021-12-09 08:40:33 +0100

    updated 2021

---
+++ 2021/aoc/point.py
@@ -0,0 +1,12 @@
+class point(object):
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return '(%d,%d)' % (self.x, self.y)
+    __repr__ = __str__
+
+    def __eq__(self, other):
+        return type(other) == type(self) and self.x == other.x and self.y == other.y
