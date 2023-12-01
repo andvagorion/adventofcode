1c0befcaf990b7cc4dc954563acc0c3dc842c9b7
Stefan Gaertner <sgaertner@cgs-online.de>
2023-12-01 07:45:05 +0100

    added 2023

---
+++ 2023/util/util.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const load = (day) => {
+    return fs.readFileSync(__dirname + `/../input/${day}`, "utf8");
+};
+
+exports.load = load;
+
+const as_lines = (day) => {
+    return load(day)
+        .split("\n")
+        .filter((line) => line != "");
+};
+
+exports.as_lines = as_lines;
+
+const range = (len) => [...Array(len).keys()];
+
+exports.range = range;
+
+const reverse_str = (str) => str.split("").reverse().join("");
+
+exports.reverse_str = reverse_str;
+
+const parse_int = (str) => parseInt(str);
+
+exports.parse_int = parse_int;
